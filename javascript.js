5f4a359e31403a7b251447c092df6a5e1f42456a
EliasBach <el.bach1999@gmail.com>
2025-03-19 21:45:30 +0100

    grid boxes change color when mouse hovers

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 // create grid based on rows and cols dimension
-let num_cols = 28
-let num_rows = 20
+let num_cols = 50
+let num_rows = 50
 grid_container = document.querySelector(".container")
 
 for (let i=0; i<num_cols; i++) {
@@ -9,6 +9,11 @@ for (let i=0; i<num_cols; i++) {
     for (let j=0; j<num_rows; j++) {
         let new_box = document.createElement("div")
         new_box.classList.add("grid-box")
+        
+        new_box.addEventListener('mouseenter', (event) => {
+            new_box.style.backgroundColor = "blue" 
+        });
+        
         new_col.appendChild(new_box)
     }
     grid_container.appendChild(new_col)
