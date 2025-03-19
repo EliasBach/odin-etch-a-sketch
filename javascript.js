dd0085fd5d43f42b358a98ffa575477ca45f91d4
EliasBach <el.bach1999@gmail.com>
2025-03-19 20:56:12 +0100

    sets up grid based on num_rows and num_cols

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// create grid based on rows and cols dimension
+let num_cols = 28
+let num_rows = 20
+grid_container = document.querySelector(".container")
+
+for (let i=0; i<num_cols; i++) {
+    let new_col = document.createElement("div")
+    new_col.classList.add("column")
+    for (let j=0; j<num_rows; j++) {
+        let new_box = document.createElement("div")
+        new_box.classList.add("grid-box")
+        new_col.appendChild(new_box)
+    }
+    grid_container.appendChild(new_col)
+}
