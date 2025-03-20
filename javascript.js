fd36ac51fdfe374d3fb4769ad9c212563ac04863
EliasBach <el.bach1999@gmail.com>
2025-03-20 21:09:27 +0100

    optimised code so that grid square size is calculated once outside for loop rather than each time when creating a square

--- javascript.js
+++ javascript.js
@@ -2,26 +2,30 @@
 const default_num_cols = 50
 const default_num_rows = 50
 const max_col_row = 100
-const grid_container_maxpxheight = 800
+const grid_container_maxpxheight = 1000
+const grid_container = document.querySelector(".container")
 
 // SCRIPT
+grid_container.style.height = `${grid_container_maxpxheight}px`
+grid_container.style.width = `${grid_container_maxpxheight}px`
 createGrid(default_num_cols, default_num_rows)
 
-// create grid based on rows and cols dimension
+// create grid based on rows and cols dimensions
 function createGrid(num_cols, num_rows) {
-    grid_container = document.querySelector(".container")
+    let grid_square_size = (grid_container_maxpxheight/num_cols)-2
 
     for (let i=0; i<num_cols; i++) {
         let new_col = document.createElement("div")
         new_col.classList.add("column")
+        
         for (let j=0; j<num_rows; j++) {
             let new_box = document.createElement("div")
             new_box.classList.add("grid-box")
             
             // boxes are sized to fit the outer grid borders
             // 2px are subtracted to account for 1px borders on both sides
-            new_box.style.height = `${(grid_container_maxpxheight/num_cols)-2}px`
-            new_box.style.width =`${(grid_container_maxpxheight/num_cols)-2}px`
+            new_box.style.height = `${grid_square_size}px`
+            new_box.style.width =`${grid_square_size}px`
             new_box.addEventListener('mouseenter', (event) => {
                 new_box.style.backgroundColor = "blue" 
             });
